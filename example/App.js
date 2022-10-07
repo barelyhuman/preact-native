a74b214c5eb735edb9fcfc0d41c335a99dd2fdef
Reaper <ahoy@barelyhuman.dev>
2022-10-07 10:30:52 +0530

    simplify example

--- example/App.js
+++ example/App.js
@@ -4,20 +4,14 @@ import { Component, h, render } from 'preact'
 let document
 
 function App({ rootTag }) {
+  // Register native components as dom compatible elements
   registerNativeDOM()
+  // create a dom with the root container from react native
   document = createDOM(rootTag)
   global.document = document
-  onRender()
+  // render a preact component to the above DOM
+  render(h(RenderableComponent, {}), document)
   return null
-}
-
-function onRender() {
-  try {
-    const jsx = h(RenderableComponent, {})
-    render(jsx, document)
-  } catch (err) {
-    console.log(err)
-  }
 }
 
 class RenderableComponent extends Component {
@@ -26,13 +20,13 @@ class RenderableComponent extends Component {
   }
 
   componentDidMount() {
-    setInterval(()=>{
-      this.setState({ count: this.state.count + 1 }) 
-    },1000)
+    setInterval(() => {
+      this.setState({ count: this.state.count + 1 })
+    }, 1000)
   }
 
   render() {
-    return h('Text', {}, `Count ${this.state.count}`)
+    return h('SafeAreaView', {}, h('Text', {}, `Count ${this.state.count}`))
   }
 }
 
