542ea032ae14b674bf93b21f8b1649398be7d104
Reaper <ahoy@barelyhuman.dev>
2022-10-24 16:09:48 +0530

    add render from the same codebase

--- src/preact.js
+++ src/preact.js
@@ -1,8 +1,8 @@
 import { createDOM } from './dom'
 import { registerNativeDOM } from './register-native-dom'
-import { h } from 'preact'
+import { h, render } from 'preact'
 
-export function withPreact(Component, render) {
+export function withPreact(Component) {
   return ({ rootTag }) => {
     registerNativeDOM()
     global.document = createDOM(rootTag)
